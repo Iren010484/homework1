fcdd62fb78f6f17f6a7310102381995223deeb40
Iren <Iren010484@mail.ru>
2023-06-16 13:32:25 +0500

    Решение третьей задачи

---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿//Программа на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+Console.Clear(); // очистка экрана
+Console.Write("Введите первое число => "); //приглашение ввода для пользователя
+string num = Console.ReadLine(); //Ввод пользователем строки
+int number = Convert.ToInt32(num); //Перевод строки в число
+int remains = number % 2; //Находим остаток от деления
+if (remains == 0)
+{Console.WriteLine ($"Число {number} - четное");}
+else
+{{Console.WriteLine ($"Число {number} - нечетное");}}
