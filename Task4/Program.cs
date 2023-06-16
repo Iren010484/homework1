100a84ebabcd5108e2ccac9fb7081b8eaa4995ec
Iren <Iren010484@mail.ru>
2023-06-16 14:04:09 +0500

    Решение четвертой задачи

---
+++ Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿//программа на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+Console.Clear(); // очистка экрана
+Console.Write("Введите число N =>   "); //приглашение ввода для пользователя
+string num = Console.ReadLine(); //Ввод пользователем строки
+int N = Convert.ToInt32(num); //Перевод строки в число
+int current = 0;
+while (current < N-1)
+{
+ Console.WriteLine(current + 2);
+ current = current + 2; 
+} 
+Console.WriteLine ($"Вывели все четные числа от 0 до {N}");
